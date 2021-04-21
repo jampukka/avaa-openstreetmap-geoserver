68f86150632041b9300f620ebdab4d2f704f8639
jampukka <janne.z.heikkila@gmail.com>
2021-04-21 22:46:45 +0300

    Change zoomStop to 11, add bounding box for seeding

--- data-update-scripts/osm-cache-seed.sh
+++ data-update-scripts/osm-cache-seed.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# generoidaan cachen tasot 1-10
+# generoidaan cachen tasot 1-11
 #
 
 echo -e "\n*** OSM Finland cache:n generointi seuraaville:\n"  >osm-cache-status
@@ -9,7 +9,7 @@ for f in osm-finland roads
 do
  echo "--- "  $f  >>osm-cache-status
  curl -s -u '<geoserver_user_name>:<geoserver_password>' -XPOST -H "Content-type: application/json"  \
-     -d "{'seedRequest':{'zoomStart':1,'zoomStop':10,'format':'image\/png','threadCount':1}}}" \
+     -d "{'seedRequest':{'zoomStart':1,'zoomStop':11,'minX':25472000,'maxX':25515000,'minY':6655000,'maxY':6699000,'format':'image\/png','threadCount':1}}}" \
   "http://localhost:8080/geoserver/gwc/rest/seed/osm_finland:$f.json"  2>>/dev/null 
 
 done 
